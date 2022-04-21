f262e3f8e16f44fbe359a6b744372a628918a7cd
Omil Dudhat <98299264+ynova019@users.noreply.github.com>
2022-04-20 23:29:14 -0500

    Update bootstrap.esm.js

--- js/bootstrap.esm.js
+++ js/bootstrap.esm.js
@@ -1,19 +1,10 @@
-/*!
-  * Bootstrap v5.1.3 (https://getbootstrap.com/)
-  * Copyright 2011-2021 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
-  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
-  */
+
 import * as Popper from '@popperjs/core';
 
-/**
- * --------------------------------------------------------------------------
- * Bootstrap (v5.1.3): util/index.js
- * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
- * --------------------------------------------------------------------------
- */
+
 const MAX_UID = 1000000;
 const MILLISECONDS_MULTIPLIER = 1000;
-const TRANSITION_END = 'transitionend'; // Shoutout AngusCroll (https://goo.gl/pxwQGp)
+const TRANSITION_END = 'transitionend'; 
 
 const toType = obj => {
   if (obj === null || obj === undefined) {
@@ -22,11 +13,7 @@ const toType = obj => {
 
   return {}.toString.call(obj).match(/\s([a-z]+)/i)[1].toLowerCase();
 };
-/**
- * --------------------------------------------------------------------------
- * Public Util Api
- * --------------------------------------------------------------------------
- */
+
 
 
 const getUID = prefix => {
